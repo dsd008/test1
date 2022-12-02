a912fd573852b7029f0b0039c1d23dc305549e0f
Singh <dhirendrasingh@kpmg.com>
2022-12-03 04:28:25 +0530

    test1

---
+++ src/main/java/in/kpmg/jwt/TestJwtDemoApplication.java
@@ -0,0 +1,13 @@
+package in.kpmg.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJwtDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestJwtDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/kpmg/jwt/TestJwtDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package in.kpmg.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestJwtDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
